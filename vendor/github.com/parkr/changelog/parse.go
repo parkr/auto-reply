876d7b7a636158da05bdfdc8fcb24d447f1f4474
Parker Moore <parkrmoore@gmail.com>
2016-03-08 17:04:27 -0600

    Update github.com/parkr/changelog

--- vendor/github.com/parkr/changelog/parse.go
+++ vendor/github.com/parkr/changelog/parse.go
@@ -9,9 +9,10 @@ import (
 )
 
 var (
-	versionRegexp    = regexp.MustCompile(`## (?i:(HEAD|\d+.\d+(.\d+)?)( / (\d{4}-\d{2}-\d{2}))?)`)
-	subheaderRegexp  = regexp.MustCompile(`### ([0-9A-Za-z_ ]+)`)
-	changeLineRegexp = regexp.MustCompile(`\* (.+)( \(((#[0-9]+)|(@[[:word:]]+))\))?`)
+	versionRegexp           = regexp.MustCompile(`## (?i:(HEAD|v?\d+.\d+(.\d+)?)( / (\d{4}-\d{2}-\d{2}))?)`)
+	subheaderRegexp         = regexp.MustCompile(`### ([0-9A-Za-z_ ]+)`)
+	changeLineRegexp        = regexp.MustCompile(`\* (.+)`)
+	changeLineRegexpWithRef = regexp.MustCompile(`\* (.+)( \(((#[0-9]+)|([[:word:]]+))\))`)
 
 	verbose = false
 )
@@ -28,6 +29,26 @@ func logVerbose(args ...interface{}) {
 	}
 }
 
+func matchLine(regexp *regexp.Regexp, line string) (matches []string, doesMatch bool) {
+	if regexp.MatchString(line) {
+		return regexp.FindStringSubmatch(line), true
+	}
+	return nil, false
+}
+
+func versionFromMatches(matches []string) *Version {
+	var date string
+	if len(matches) == 5 {
+		date = matches[4]
+	}
+	return &Version{
+		Version:     matches[1],
+		Date:        date,
+		History:     []*ChangeLine{},
+		Subsections: []*Subsection{},
+	}
+}
+
 func parseChangelog(file io.Reader, history *Changelog) error {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
@@ -39,28 +60,17 @@ func parseChangelog(file io.Reader, history *Changelog) error {
 		txt := scanner.Text()
 		logVerbose(txt)
 		logVerbose("isHeader", versionRegexp.MatchString(txt))
-		if versionRegexp.MatchString(txt) {
-			matches := versionRegexp.FindStringSubmatch(txt)
+		if matches, ok := matchLine(versionRegexp, txt); ok {
 			logVerbose("headerMatches:", matches, len(matches))
 			currentHeader = matches[1]
 			currentSubHeader = ""
 			logVerbose("currentHeader: '%s'", currentHeader)
-			var date string
-			if len(matches) == 5 {
-				date = matches[4]
-			}
-			history.Versions = append(history.Versions, &Version{
-				Version:     currentHeader,
-				Date:        date,
-				History:     []*ChangeLine{},
-				Subsections: []*Subsection{},
-			})
+			history.Versions = append(history.Versions, versionFromMatches(matches))
 			continue
 		}
 
 		logVerbose("isSubHeader", subheaderRegexp.MatchString(txt))
-		if subheaderRegexp.MatchString(txt) {
-			matches := subheaderRegexp.FindStringSubmatch(txt)
+		if matches, ok := matchLine(subheaderRegexp, txt); ok {
 			logVerbose("subHeaderMatches:", matches, len(matches))
 			currentSubHeader = matches[1]
 			logVerbose("currentSubHeader: '%s'", currentSubHeader)
@@ -69,12 +79,20 @@ func parseChangelog(file io.Reader, history *Changelog) error {
 		}
 
 		logVerbose("isChangeLine", changeLineRegexp.MatchString(txt))
-		if changeLineRegexp.MatchString(txt) {
-			matches := changeLineRegexp.FindStringSubmatch(txt)
+		if matches, ok := matchLine(changeLineRegexp, txt); ok {
 			logVerbose("changeLineMatches:", matches, len(matches))
-			line := &ChangeLine{
-				Summary:   matches[1],
-				Reference: matches[3],
+			var line *ChangeLine
+			if more, ok := matchLine(changeLineRegexpWithRef, txt); ok {
+				// Has ref
+				line = &ChangeLine{
+					Summary:   more[1],
+					Reference: more[3],
+				}
+			} else {
+				// No ref
+				line = &ChangeLine{
+					Summary: matches[1],
+				}
 			}
 			logVerbose("newChangeLine:", line)
 			currentLine = line
