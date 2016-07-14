1b53039e223d47d10cb830ac33a06349b8b22f98
Parker Moore <parkrmoore@gmail.com>
2016-07-13 17:59:01 -0700

    I should be allowed to use exclamation marks, c'mon.

--- lgtm/lgtm_test.go
+++ lgtm/lgtm_test.go
@@ -6,12 +6,16 @@ import (
 
 func TestLGTMBodyRegexp(t *testing.T) {
 	cases := map[string]bool{
-		"lgtm":               true,
-		"LGTM":               true,
-		"LGTM.":              true,
-		"@jekyllbot: LGTM":   true,
-		"Yeah, this LGTM.":   true,
-		"Then I'll LGTM it.": false,
+		"lgtm":                                  true,
+		"LGTM":                                  true,
+		"LGTM.":                                 true,
+		"LGTM!":                                 true,
+		"LGTM!!!":                               true,
+		"@jekyllbot: LGTM":                      true,
+		"Yeah, this LGTM.":                      true,
+		"Then I'll LGTM it.":                    false,
+		"I'd love to get a LGTM for this.":      false,
+		"@envygeeks, can you give this a LGTM?": false,
 	}
 	for input, expected := range cases {
 		if actual := lgtmBodyRegexp.MatchString(input); actual != expected {
