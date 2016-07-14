eabd48afc1fedc0a26ec25aa260d4c6b96e6b1ee
Parker Moore <parkrmoore@gmail.com>
2016-07-13 18:21:20 -0700

    Enable for way more repos.

--- jekyll/jekyll.go
+++ jekyll/jekyll.go
@@ -11,7 +11,25 @@ import (
 	"github.com/parkr/auto-reply/jekyll/issuecomment"
 )
 
-var lgtmEnabledRepos = []lgtm.Repo{{Owner: "jekyll", Name: "jekyll-import", Quorum: 1}}
+var lgtmEnabledRepos = []lgtm.Repo{
+	//{Owner: "jekyll", Name: "jekyll", Quorum: 2},
+	{Owner: "jekyll", Name: "jekyll-import", Quorum: 1},
+	{Owner: "jekyll", Name: "jekyll-feed", Quorum: 2},
+	{Owner: "jekyll", Name: "jekyll-sitemap", Quorum: 2},
+	{Owner: "jekyll", Name: "jekyll-mentions", Quorum: 2},
+	{Owner: "jekyll", Name: "jekyll-watch", Quorum: 2},
+	{Owner: "jekyll", Name: "jekyll-compose", Quorum: 2},
+	{Owner: "jekyll", Name: "jekyll-paginate", Quorum: 2},
+	{Owner: "jekyll", Name: "jekyll-gist", Quorum: 2},
+	{Owner: "jekyll", Name: "jekyll-coffeescript", Quorum: 2},
+	{Owner: "jekyll", Name: "jekyll-opal", Quorum: 2},
+	{Owner: "jekyll", Name: "jekyll-sass-converter", Quorum: 2},
+	{Owner: "jekyll", Name: "jekyll-textile-converter", Quorum: 2},
+	{Owner: "jekyll", Name: "jekyll-redirect-from", Quorum: 2},
+	{Owner: "jekyll", Name: "github-metadata", Quorum: 2},
+	{Owner: "jekyll", Name: "jemoji", Quorum: 1},
+	{Owner: "jekyll", Name: "mercenary", Quorum: 1},
+}
 
 var jekyllOrgEventHandlers = map[hooks.EventType][]hooks.EventHandler{
 	hooks.IssuesEvent: {deprecate.DeprecateOldRepos},
