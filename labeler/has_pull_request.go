27199751d69fcd80f39827f471767e325a0fedf2
Thiago Arrais <thiago.arrais@gmail.com>
2016-12-27 20:37:22 -0300

    first try to detect the fixed issue

--- labeler/has_pull_request.go
+++ labeler/has_pull_request.go
@@ -1,56 +1,41 @@
 package labeler
 
 import (
-	"fmt"
 	"log"
-	"time"
+	"regexp"
 
 	"github.com/google/go-github/github"
-	"github.com/parkr/auto-reply/common"
 	"github.com/parkr/auto-reply/ctx"
 )
 
-const repoMergeabilitayCheckWaitSec = 2
+var fixesIssueMatcher = regexp.MustCompile(`Fixes #(\d+)`)
 
 var IssueHasPullRequestLabeler = func(context *ctx.Context, payload interface{}) error {
 	event, ok := payload.(*github.PullRequestEvent)
 	if !ok {
-		return context.NewError("PendingRebaseUnlabeler: not a pull request event")
+		return context.NewError("IssueHasPullRequestLabeler: not a pull request event")
 	}
 
-	if *event.Action != "synchronize" {
+	if *event.Action != "opened" {
 		return nil
 	}
 
-	owner, repo, num := *event.Repo.Owner.Login, *event.Repo.Name, *event.Number
-
-	// Allow the job to run which determines mergeability.
-	log.Printf("checking the mergeabilitay of %s/%s#%d in %d sec...", owner, repo, num, repoMergeabilitayCheckWaitSec)
-	time.Sleep(repoMergeabilitayCheckWaitSec * time.Second)
+        description := *event.PullRequest.Body
+	issueNum := issueFixed(description)
 
 	var err error
-	if isMergeablee(context, owner, repo, num) {
-		err = RemoveLabelIfExists(context.GitHub, owner, repo, num, "has-pull-request")
-		if err != nil {
-			log.Printf("error removing the has-pull-request label: %v", err)
-		}
-		err = RemoveLabelIfExists(context.GitHub, owner, repo, num, "needs-work")
-	} else {
-		err = fmt.Errorf("%s/%s#%d is not mergeable", owner, repo, num)
+	if issueNum != "" {
+		log.Printf("detected a pull request that fixes issue %v", issueNum)
 	}
 
-	if err != nil {
-		log.Printf("error removing the pending-rebase & needs-work labels: %v", err)
-	}
 	return err
 }
 
-func isMergeablee(context *ctx.Context, owner, repo string, number int) bool {
-	pr, res, err := context.GitHub.PullRequests.Get(owner, repo, number)
-	err = common.ErrorFromResponse(res, err)
-	if err != nil {
-		log.Printf("couldn't determine mergeability of %s/%s#%d: %v", owner, repo, number, err)
-		return false
+func issueFixed(description string) string {
+	issueSubmatches := fixesIssueMatcher.FindAllStringSubmatch(description, -1)
+	if len(issueSubmatches) == 0 || len(issueSubmatches[0]) < 2 {
+		return ""
 	}
-	return *pr.Mergeable
+
+	return issueSubmatches[0][1]
 }
