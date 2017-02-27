44da977dfca20eff9772957860d73613a6bdbc47
Parker Moore <parkrmoore@gmail.com>
2017-02-27 15:49:11 -0500

    Give us the ability to pass a list of issues (either as URL or owner/repo#number) to freeze-ancient-issues

--- cmd/freeze-ancient-issues/main.go
+++ cmd/freeze-ancient-issues/main.go
@@ -3,10 +3,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/google/go-github/github"
 	"github.com/parkr/auto-reply/ctx"
 	"github.com/parkr/auto-reply/freeze"
 )
@@ -54,6 +58,14 @@ func main() {
 		log.Fatalln("cannot proceed without github client")
 	}
 
+	// Support running on just a list of issues. Either a URL or a `owner/name#number` syntax.
+	if flag.NArg() > 0 {
+		if err := processSingleIssues(context, actuallyDoIt, flag.Args()...); err != nil {
+			log.Fatalf("error: %#v", err)
+		}
+		return
+	}
+
 	var wg sync.WaitGroup
 	for _, repo := range repos {
 		wg.Add(1)
@@ -68,6 +80,60 @@ func main() {
 	wg.Wait()
 }
 
+func extractIssueInfo(issueName string) (owner, repo string, number int) {
+	issueName = strings.TrimPrefix(issueName, "https://github.com/")
+
+	var err error
+	pieces := strings.Split(issueName, "/")
+
+	// Ex: `owner/repo#number`
+	if len(pieces) == 2 {
+		owner = pieces[0]
+		morePieces := strings.Split(pieces[1], "#")
+		if len(morePieces) == 2 {
+			repo = morePieces[0]
+			number, err = strconv.Atoi(morePieces[1])
+			if err != nil {
+				log.Printf("huh? %#v for %s", err, morePieces[1])
+			}
+		}
+		return
+	}
+
+	// Ex: `owner/repo/issues/number`
+	if len(pieces) == 4 {
+		owner = pieces[0]
+		repo = pieces[1]
+		number, err = strconv.Atoi(pieces[3])
+		if err != nil {
+			log.Printf("huh? %#v for %s", err, pieces[3])
+		}
+		return
+	}
+
+	return "", "", 0
+}
+
+func processSingleIssues(context *ctx.Context, actuallyDoIt bool, issueNames ...string) error {
+	issues := []github.Issue{}
+	for _, issueName := range issueNames {
+		owner, repo, number := extractIssueInfo(issueName)
+		if owner == "" || repo == "" || number <= 0 {
+			return fmt.Errorf("couldn't extract issue info from '%s': owner=%s repo=%s number=%d",
+				issueName, owner, repo, number)
+		}
+
+		issues = append(issues, github.Issue{
+			Number: github.Int(number),
+			Repository: &github.Repository{
+				Owner: &github.User{Login: github.String(owner)},
+				Name:  github.String(repo),
+			},
+		})
+	}
+	return processIssues(context, actuallyDoIt, issues)
+}
+
 func processRepo(context *ctx.Context, owner, repo string, actuallyDoIt bool) error {
 	start := time.Now()
 
@@ -76,11 +142,19 @@ func processRepo(context *ctx.Context, owner, repo string, actuallyDoIt bool) er
 		return err
 	}
 
-	log.Printf("%s/%s: Freezing %d closed issues before %v", owner, repo, len(issues), freeze.TooOld)
+	log.Printf("%s/%s: freezing %d closed issues before %v", owner, repo, len(issues), freeze.TooOld)
+	err = processIssues(context, actuallyDoIt, issues)
+	log.Printf("%s/%s: finished in %s", owner, repo, time.Since(start))
+
+	return err
+}
+
+func processIssues(context *ctx.Context, actuallyDoIt bool, issues []github.Issue) error {
 	for _, issue := range issues {
+		owner, repo := *issue.Repository.Owner.Login, *issue.Repository.Name
 		if actuallyDoIt {
 			log.Printf("%s/%s: freezing %s", owner, repo, *issue.HTMLURL)
-			if err = freeze.Freeze(context, owner, repo, *issue.Number); err != nil {
+			if err := freeze.Freeze(context, owner, repo, *issue.Number); err != nil {
 				return err
 			}
 			time.Sleep(sleepBetweenFreezes)
@@ -89,8 +163,5 @@ func processRepo(context *ctx.Context, owner, repo string, actuallyDoIt bool) er
 			time.Sleep(sleepBetweenFreezes)
 		}
 	}
-
-	log.Printf("%s/%s: finished in %s", owner, repo, time.Since(start))
-
 	return nil
 }
