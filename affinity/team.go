9005fbc9a1844a24d56bb15a9bef39b42d1644ed
Parker Moore <parkrmoore@gmail.com>
2017-07-30 16:55:12 -0400

    affinity: refactor Team code to remove cruft and speed up the process

--- affinity/team.go
+++ affinity/team.go
@@ -6,16 +6,15 @@ import (
 	"time"
 
 	"github.com/google/go-github/github"
-	"github.com/parkr/auto-reply/auth"
 	"github.com/parkr/auto-reply/ctx"
 )
 
 func NewTeam(context *ctx.Context, teamId int) (Team, error) {
 	team := Team{ID: teamId}
-	if err := team.FetchMetadata(context); err != nil {
+	if err := team.fetchMetadata(context); err != nil {
 		return Team{}, err
 	}
-	if err := team.FetchCaptains(context); err != nil {
+	if err := team.fetchCaptains(context); err != nil {
 		return Team{}, err
 	}
 
@@ -56,70 +55,59 @@ func (t Team) String() string {
 func (t Team) RandomCaptainLogins(num int) []string {
 	rand.Seed(time.Now().UnixNano())
 
-	selections := []string{}
+	selectionmap := map[string]bool{}
 
 	// Just return all of them.
 	if len(t.Captains) <= num {
-		for _, captain := range t.Captains {
-			selections = append(selections, *captain.Login)
-		}
-		return selections
+		return usersByLogin(t.Captains)
 	}
 
 	// Find a random selection.
-OuterLoop:
 	for {
 		selection := t.Captains[rand.Intn(len(t.Captains))]
-		for _, previous := range selections {
-			if *selection.Login == previous {
-				continue OuterLoop
-			}
-		}
-		selections = append(selections, *selection.Login)
+		selectionmap[selection.GetLogin()] = true
 
-		if len(selections) == num {
+		if len(selectionmap) == num {
 			break
 		}
+	}
+
+	selections := []string{}
+	for login, _ := range selectionmap {
+		selections = append(selections, login)
 	}
 	return selections
 }
 
 func (t Team) RandomCaptainLoginsExcluding(excludedLogin string, count int) []string {
 	var selections []string
-	validSelection := true
 
-	// Try 10 times to get a set of captains which doesn't include the
-	// excluded one.
-	for i := 0; i < 10; i++ {
-		selections = t.RandomCaptainLogins(count)
-
-		// Have we included the login of the excluded captain?
-		validSelection = true
-		for _, selection := range selections {
-			if excludedLogin == selection {
-				validSelection = false
+	// If the pool of captains isn't big enough to hit count without the excluded login,
+	// then just return all the other captains.
+	if len(t.Captains)-1 <= count {
+		for _, user := range t.Captains {
+			if user.GetLogin() != excludedLogin {
+				selections = append(selections, user.GetLogin())
 			}
 		}
+		return selections
+	}
 
-		// If everything is OK, then return.
-		if validSelection {
+	// We can only ever exclude 1 login, so just get count + 1 random logins
+	// and pick them one by one until we get our desired count.
+	for _, login := range t.RandomCaptainLogins(count + 1) {
+		if login != excludedLogin {
+			selections = append(selections, login)
+		}
+		if len(selections) == count {
 			break
 		}
 	}
 
-	if !validSelection {
-		var limitedSelection []string
-		for _, selection := range selections {
-			if selection != excludedLogin {
-				limitedSelection = append(limitedSelection, selection)
-			}
-		}
-		return limitedSelection
-	}
 	return selections
 }
 
-func (t *Team) FetchCaptains(context *ctx.Context) error {
+func (t *Team) fetchCaptains(context *ctx.Context) error {
 	users, _, err := context.GitHub.Organizations.ListTeamMembers(
 		context.Context(),
 		t.ID,
@@ -132,43 +120,16 @@ func (t *Team) FetchCaptains(context *ctx.Context) error {
 		return err
 	}
 
-	t.Captains = users
-
-	if t.Org != "" {
-		allMembers, _, err := context.GitHub.Organizations.ListTeamMembers(
-			context.Context(),
-			t.ID,
-			&github.OrganizationListTeamMembersOptions{
-				Role:        "all",
-				ListOptions: github.ListOptions{Page: 0, PerPage: 100},
-			},
-		)
-		if err != nil {
-			return err
+	for _, user := range users {
+		if !context.GitHubAuthedAs(user.GetLogin()) {
+			t.Captains = append(t.Captains, user)
 		}
-
-		for _, user := range allMembers {
-			if !t.IsCaptain(*user.Login) && auth.UserIsOrgOwner(context, t.Org, *user.Login) && !context.GitHubAuthedAs(*user.Login) {
-				t.Captains = append(t.Captains, user)
-			}
-		}
-	} else {
-		context.Log("Team.FetchCaptains: cannot fetch org owners without Team.Org value")
 	}
 
 	return nil
 }
 
-func (t *Team) IsCaptain(login string) bool {
-	for _, captain := range t.Captains {
-		if *captain.Login == login {
-			return true
-		}
-	}
-	return false
-}
-
-func (t *Team) FetchMetadata(context *ctx.Context) error {
+func (t *Team) fetchMetadata(context *ctx.Context) error {
 	team, _, err := context.GitHub.Organizations.GetTeam(context.Context(), t.ID)
 	if err != nil {
 		return err
@@ -179,13 +140,4 @@ func (t *Team) FetchMetadata(context *ctx.Context) error {
 	t.Mention = fmt.Sprintf("@%s/%s", t.Org, *team.Slug)
 	t.Description = *team.Description
 	return nil
-}
-
-func (t *Team) IsTeamCaptain(login string) bool {
-	for _, captain := range t.Captains {
-		if *captain.Login == login {
-			return true
-		}
-	}
-	return false
 }
